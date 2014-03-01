5a6b22dbde6e8f36d2b57089c71f9b789b13ec83
Evgeniy Kelyarsky <evgenek@wix.com>
2014-03-01 17:53:58 +0200

    initialize_on_precompile = false for heroku

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module ForbabiesDpUa
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
+    config.assets.initialize_on_precompile = false
     I18n.enforce_available_locales = false
   end
 end
